d8c11c6f51a1ca653e6b78ff5e3e149e5b950482
sebastian <sebastiancampos512190@gmail.com>
2026-07-23 16:43:44 -0700

    subir

--- servidor base/crud_peliculas/script.js
+++ servidor base/crud_peliculas/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/peliculas";
+const API_URL = "https://pw-verano2026-beta.vercel.app/";
 
 const formulario = document.getElementById("formulario");
 const id = document.getElementById("id");
